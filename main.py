b0686430c7ad7f8d071e1b28377de3fe86420af1
Gabriel Cabangon <earthlycookies@gmail.com>
2026-09-21 22:08:52 +0800

    Add class section to main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print(Lab 1: Version Control)
-print ("Author: [Gabriel Cabangon]")
-print ("Student ID: TUPM-26-7964")
+print("Lab 1: Version Control")
+print("Author: [Gabriel Cabangon]")
+print("Student ID: TUPM-26-7964")
+print("Class Section: [BSECE-1A]")
